559ccd94801ec87f6762db5d0ba1b8982c8f48f8
Yuval <yuvaldamti12@gmail.com>
2026-01-25 16:50:24 +0200

    Moved sudoku solving setup to solver class

--- OmegaSudoku/Program.cs
+++ OmegaSudoku/Program.cs
@@ -2,32 +2,11 @@
 
 try
 {
-    SudokuBoard board = new SudokuBoard(9);
     //int[] sudokuArr = InputValidation.ConvertInput("800000000003600000070090200050007000000045700000100030001000068008500010090000400");
-    int[] sudokuArr = InputValidation.ConvertInput("000000068900000002000400500041000000000035000050000000000800010300000700000100400");
-    bool valid = board.LoadBoard(sudokuArr);
+    //int[] sudokuArr = InputValidation.ConvertInput("000000068900000002000400500041000000000035000050000000000800010300000700000100400");
 
-    Console.WriteLine("Initial board:");
-    board.PrintBoard();
-
-    BacktrackingSolver solver = new BacktrackingSolver(board);
-
-    DateTime before = DateTime.Now;
-
-    bool solved = solver.SolveBacktracking();
-
-    DateTime after = DateTime.Now;
-
-    TimeSpan time = after - before;
-
-    if (solved)
-    {
-        Console.WriteLine("Solved board:");
-        board.PrintBoard();
-
-        Console.WriteLine("\nSolve time:");
-        Console.WriteLine(time.ToString());
-    }
+    SudokuSolver.SolveSudoku("000000068900000002000400500041000000000035000050000000000800010300000700000100400");
+    //SudokuSolver.SolveSudoku("800000000003600000070090200050007000000045700000100030001000068008500010090000400");
 }
 catch (Exception e)
 {
---
+++ OmegaSudoku/SudokuSolver.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaSudoku
+{
+    public static class SudokuSolver
+    {
+        public static string SolveSudoku(string sudokuString)
+        {
+            // Convert the input string to an array
+            int[] sudokuArr = InputValidation.ConvertInput(sudokuString);
+
+            // Create new sudoku board and load the array into it
+            SudokuBoard board = new SudokuBoard(9);
+            bool valid = board.LoadBoard(sudokuArr);
+
+            // Print the initial board
+            Console.WriteLine("Initial board:");
+            board.PrintBoard();
+
+            if (!valid)
+            {
+                // Board is invalid
+                // TODO: throw custom exception
+                return "";
+            }
+
+            // Create the backtracking solver object
+            BacktrackingSolver solver = new BacktrackingSolver(board);
+
+            // Messure the time before solving
+            DateTime before = DateTime.Now;
+
+            // Run the solver algorithm
+            bool solved = solver.SolveBacktracking();
+
+            // Messure the time after solving
+            DateTime after = DateTime.Now;
+
+            // Calculate solving time
+            TimeSpan time = after - before;
+
+            if (!solved)
+            {
+                // Board is unsolveable
+                // TODO: throw custom exception
+                return "";
+            }
+
+            // Print the solved board
+            Console.WriteLine("Solved board:");
+            board.PrintBoard();
+
+            // Print the solve time
+            Console.WriteLine("\nSolve time:");
+            Console.WriteLine(time.ToString());
+            Console.WriteLine();
+
+            // Temporary return
+            // Will return solved board in the same format as the input board
+            return "";
+        }
+    }
+}
