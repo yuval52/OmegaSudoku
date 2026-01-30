609f7d8b28bf4947e7a8f384cb098ddf061139e1
yuval52 <yuvaldamti12@gmail.com>
2026-01-31 01:38:48 +0200

    Added documentation to the SudokuApp class

--- OmegaSudoku/SudokuApp.cs
+++ OmegaSudoku/SudokuApp.cs
@@ -2,24 +2,34 @@
 
 namespace OmegaSudoku
 {
+    /// <summary>
+    /// The main class for running the Omega Sudoku application.
+    /// </summary>
     public static class SudokuApp
     {
+        /// <summary>
+        /// Starts the Omega Sudoku application.
+        /// </summary>
         public static void Run()
         {
             Console.WriteLine("Welcome to the Omega sudoku solver!\n");
             while (true)
             {
+                // The main loop for repeatedly running the Sudoku solver
                 try
                 {
                     string output = UserInterface.RunSudokuSolver();
                     if (output.Equals("quit"))
                     {
+                        // User quit the application
                         Console.WriteLine("Quitting the program...");
+                        // Exit the loop and end the program
                         break;
                     }
                 }
                 catch (Exception e)
                 {
+                    // Inform the user of exceptions encountered during application running
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("----------------------------------------------------------------");
                     Console.WriteLine("Encountered an error:");
