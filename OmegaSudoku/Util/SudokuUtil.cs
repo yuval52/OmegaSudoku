c1ff38f0e2e0df3616ebcd8cfd52813fabc16901
yuval52 <yuvaldamti12@gmail.com>
2026-01-31 00:02:15 +0200

    Added documentation to the SudokuUtil class

--- OmegaSudoku/Util/SudokuUtil.cs
+++ OmegaSudoku/Util/SudokuUtil.cs
@@ -1,8 +1,17 @@
 ﻿namespace OmegaSudoku.Util
 {
+    /// <summary>
+    /// A static utility class for common Sudoku functions.
+    /// </summary>
     public static class SudokuUtil
     {
 
+        /// <summary>
+        /// Determines the number of possible options (0 bits) in a given bitmask.
+        /// </summary>
+        /// <param name="mask">The bitmask to check.</param>
+        /// <param name="size">The max amount of bits to check.</param>
+        /// <returns>The amount of 0 bits in the bitmask.</returns>
         public static int NumberOfOptions(int mask, int size)
         {
             int countSetBits = 0;
@@ -18,18 +27,36 @@
             return size - countSetBits;
         }
 
+        /// <summary>
+        /// Checks if a number is present in the given bitmask.
+        /// </summary>
+        /// <param name="mask">The bitmask to check.</param>
+        /// <param name="num">The number to look for.</param>
+        /// <returns>Whether the number is in the bitmask.</returns>
         public static bool IsNumberInMask(int mask, int num)
         {
             int numMask = 1 << (num - 1);
             return (mask & numMask) != 0;
         }
 
+        /// <summary>
+        /// Adds a number to the given bitmask.
+        /// </summary>
+        /// <param name="mask">Thr mask to add to.</param>
+        /// <param name="num">The number to add to the mask.</param>
+        /// <returns>The new bitmask with the number added.</returns>
         public static int AddNumberToMask(int mask, int num)
         {
             int numMask = 1 << (num - 1);
             return mask | numMask;
         }
 
+        /// <summary>
+        /// Removes a number from the given bitmask.
+        /// </summary>
+        /// <param name="mask">The mask to remove from.</param>
+        /// <param name="num">The number to remove.</param>
+        /// <returns>The new mask with the number removed.</returns>
         public static int RemoveNumberFromMask(int mask, int num)
         {
             int numMask = 1 << (num - 1);
