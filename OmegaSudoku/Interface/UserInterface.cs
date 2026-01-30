c1e0a643ee941611c77aac1c5f3008c029a58f17
yuval52 <yuvaldamti12@gmail.com>
2026-01-31 00:50:49 +0200

    Added documentation to the UserInterface class

--- OmegaSudoku/Interface/UserInterface.cs
+++ OmegaSudoku/Interface/UserInterface.cs
@@ -4,13 +4,18 @@ using OmegaSudoku.Tests;
 
 namespace OmegaSudoku.Interface
 {
+    /// <summary>
+    /// A static class that handles the user interface for the Sudoku solver.
+    /// </summary>
     public static class UserInterface
     {
-
+        /// <summary>
+        /// Runs the Sudoku solver user interface once.
+        /// </summary>
+        /// <returns>The string representation of the solved Sudoku board inputted by the user.</returns>
         public static string RunSudokuSolver()
         {
             // Display the insturctions interface
-            Console.Clear();
             ShowInterface();
 
             // Wait for input
@@ -42,6 +47,12 @@ namespace OmegaSudoku.Interface
 
             return result;
         }
+
+        /// <summary>
+        /// Opens the test interface to run a large amount of Sudokus from a dataset.
+        /// </summary>
+        /// <exception cref="InputIsNotANumberException">An exception that is thrown when the input is not a number.</exception>
+        /// <exception cref="InputOutOfRangeException">An exception that is thrown when the input is not in the range of numbers possible for the test.</exception>
         public static void TestInterface()
         {
             Console.Clear();
@@ -73,8 +84,13 @@ namespace OmegaSudoku.Interface
             Console.ReadLine();
 
         }
+
+        /// <summary>
+        /// Displays the main page instrcutions.
+        /// </summary>
         public static void ShowInterface()
         {
+            Console.Clear();
             Console.WriteLine("----------------------------------------------------------------");
             Console.WriteLine("Sudoku board input\n");
             Console.WriteLine("A sudoku board is represented by a series of 81 digits");
