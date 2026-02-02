7e2d1127a1db306766da3b407d281e6de8392b69
Yuval <yuvaldamti12@gmail.com>
2026-02-02 11:35:23 +0200

    Created program class

--- OmegaSudoku/Program.cs
+++ OmegaSudoku/Program.cs
@@ -1,3 +1,16 @@
-﻿using OmegaSudoku;
-
-SudokuApp.Run();
+﻿namespace OmegaSudoku
+{
+    /// <summary>
+    /// The main program class that starts the application
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// The entry point for the program, starts the application
+        /// </summary>
+        public static void Main(string[] args)
+        {
+            SudokuApp.Run();
+        }
+    }
+}
